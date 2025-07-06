bc576b55450a7bd16359224819c0c81fa5383575
aliraza54321 <aaagames666@gmail.com>
2025-07-06 11:42:11 +0500

    Update support.js

--- api/support.js
+++ api/support.js
@@ -15,5 +15,7 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Something went wrong." });
+    console.error(error);
+    res.status(500).json({ error: "n8n error" });
   }
+}
